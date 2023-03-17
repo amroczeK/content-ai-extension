8c7499e0c8cde22fdcbfa511b02585307a17afa6
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2023-03-17 20:40:56 +0800

    cleanup

--- src/hooks/useSelection.tsx
+++ src/hooks/useSelection.tsx
@@ -16,13 +16,6 @@ export const useSelection = (): ISelection => {
 
   const selectionHandler = () => setSelection(getSelection())
 
-  // Debounce handler to improve performance, preventing re-rendering of parent component using the useSelection hook.
-  // The selection handler is debounced and the selection is set 500ms after the user has made a selection on the browser.
-  // const debouncedSelectionHandler = useMemo(
-  //   () => debounce(selectionHandler, 500),
-  //   [selection.selectedText]
-  // )
-
   // Example using useCallback
   const debouncedSelectionHandler = useCallback(
     () => debounce(selectionHandler, 500),
@@ -30,22 +23,10 @@ export const useSelection = (): ISelection => {
   )
 
   useEffect(() => {
-    // // Use this for useMemo
-    // document.addEventListener("selectionchange", () => {
-    //   debouncedSelectionHandler()
-    // })
-
-    // Use this for useCallback
     document.addEventListener("selectionchange", debouncedSelectionHandler())
-    
+
     // Clean up event listners on unmount to prevent unecessary updates or memory leaks
     return () => {
-      // // Use this for useMemo
-      // document.removeEventListener("selectionchange", () => {
-      //   debouncedSelectionHandler()
-      // })
-
-      // Use this for useCallback
       document.removeEventListener(
         "selectionchange",
         debouncedSelectionHandler()
